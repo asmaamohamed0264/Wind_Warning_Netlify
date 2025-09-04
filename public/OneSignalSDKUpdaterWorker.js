d4675961ac1bc1209d41d508d386a4ea4b067196
Mariam Ahmed <asmaa.mohamed0264@std.bus.asu.edu.eg>
2025-09-05 00:21:38 +0300

    Update OneSignalSDKUpdaterWorker.js

--- public/OneSignalSDKUpdaterWorker.js
+++ public/OneSignalSDKUpdaterWorker.js
@@ -1,4 +1,4 @@
 // OneSignal Updater Service Worker
 // This companion worker helps handle updates. Keep it alongside the main worker at the site root.
-importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-
+// v16 service worker
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
