e8596cde6b9b5513385fac0c8e9c0c8ff5cf7e58
Mariam Ahmed <asmaa.mohamed0264@std.bus.asu.edu.eg>
2025-09-10 22:56:56 +0300

    Improve OneSignal notifyButton settings

--- app/onesignal.ts
+++ app/onesignal.ts
@@ -13,6 +13,7 @@ export async function initOneSignal() {
     }
     return;
   }
+
   try {
     await OneSignal.init({
       appId: APP_ID,
@@ -20,10 +21,35 @@ export async function initOneSignal() {
       serviceWorkerPath: '/OneSignalSDKWorker.js',
       serviceWorkerUpdaterPath: '/OneSignalSDKUpdaterWorker.js',
       serviceWorkerParam: { scope: '/' },
-      notifyButton: { enable: true },
+
+      // Obiect complet pentru a satisface tipurile stricte
+      notifyButton: {
+        enable: true,
+        prenotify: true,
+        showCredit: false,
+        position: 'bottom-right', // 'bottom-right' | 'bottom-left'
+        size: 'medium',           // 'small' | 'medium' | 'large'
+        // offset: { bottom: '0px', left: '0px', right: '0px' }, // opțional
+        text: {
+          'tip.state.unsubscribed': 'Subscribe',
+          'tip.state.subscribed': 'Subscribed',
+          'tip.state.blocked': 'Blocked',
+          'message.prenotify': 'Click to subscribe to notifications',
+          'message.action.subscribed': 'Thanks for subscribing!',
+          'message.action.resubscribed': 'You are subscribed',
+          'message.action.unsubscribed': "You won't receive notifications",
+          'dialog.main.title': 'Manage notifications',
+          'dialog.main.button.subscribe': 'SUBSCRIBE',
+          'dialog.main.button.unsubscribe': 'UNSUBSCRIBE',
+          'dialog.blocked.title': 'Unblock notifications',
+          'dialog.blocked.message': 'Follow these instructions to allow notifications:',
+        },
+      },
     });
-    // opțional:
+
+    // opțional – dacă vrei prompt manual imediat:
     // await OneSignal.registerForPushNotifications();
+
   } catch (e) {
     console.error('OneSignal init error', e);
   }
